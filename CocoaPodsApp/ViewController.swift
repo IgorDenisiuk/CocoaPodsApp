262337e8eea557ce1ad1bf107c818b9717b59057
IgorDenisiuk <73896271+IgorDenisiuk@users.noreply.github.com>
2020-12-30 12:56:00 +0300

    Initial Commit

---
+++ CocoaPodsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CocoaPodsApp
+//
+//  Created by Igor Denisiuk on 12/30/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
